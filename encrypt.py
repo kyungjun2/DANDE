0710cc8ad458c9a7dfb5b49a2e163c6f5d7a5ec5
Kyungjun <kyungjun2@gmail.com>
2019-11-02 18:25:11 +0900

    - 복호화에도 멀티쓰레딩 적용 - 작업상태를 퍼센트로 표시

--- encrypt.py
+++ encrypt.py
@@ -57,7 +57,6 @@ class EncryptFile:
             return cipher.encrypt(data)
 
     def encrypt_block(self, block, number, q, process_count):
-        print("{}번 쓰레드 | 블록크기 {}".format(int(number/self.block_size), format_size(get_size(block))))
         process_count.value += 1
 
         result = None
@@ -100,6 +99,8 @@ class EncryptFile:
         count = 0
         live_process = Value('i', 0)
         length = len(raw_file)
+        maximum_thread = int(length / self.block_size) + 1
+
         process_list = []
         file_bytes = Queue()
 
@@ -107,11 +108,15 @@ class EncryptFile:
             p = Process(target=self.encrypt_block, args=(raw_file[i:i+self.block_size], i, file_bytes, live_process, ))
             p.start()
             process_list.append(p)
-
-            i += self.block_size
             count += 1
+            i += self.block_size
+
+            printed = False
             while live_process.value > self.maximum_process:
                 time.sleep(0.2)
+                if printed is False:
+                    print("%0.2f" % (((count - 1) / maximum_thread) * 100) + "%")
+                    printed = True
 
         while file_bytes.qsize() < count:
             time.sleep(0.5)
@@ -138,6 +143,8 @@ class EncryptFile:
 class DecryptFile:
     def __init__(self, key):
         self.key = key
+        self.block_size = 1024*int(1024/2)
+        self.maximum_process = 8
 
     def aes_decrypt(self, data):
         from Crypto.Cipher import AES
@@ -152,28 +159,82 @@ class DecryptFile:
         else:
             return cipher.decrypt(data)
 
+    def decrypt_block(self, block, number, q, process_count):
+        process_count.value += 1
+
+        if get_size(block) < 16:
+            result = {number: block}
+
+        else:
+            i = 0
+            decrypted_block = b''
+            length = len(block)
+
+            while i < length:
+                if i + 16 <= length:
+                    decrypted_block += self.aes_decrypt(block[i:i + 16])
+                    decrypted_block += block[i + 16:i + 32]
+                else:
+                    decrypted_block += block[i:]
+                i += 32
+
+            result = {number: decrypted_block}
+
+        q.put(result)
+        process_count.value += -1
+
     def decrypt_file(self, path):
+        from multiprocessing import Process, Queue, Value
+        import time
+
         if validate_file(path) is False:
             print("파일이 존재하지 않음")
             kill()
 
         raw_file = read_file(path)
-        new_file = b''
-
         print("파일 크기 = {0}".format(format_size(get_size(raw_file))))
         if get_size(raw_file) < 16:
             print("파일이 너무 작음")
             kill()
 
         i = 0
+        count = 0
+        live_process = Value('i', 0)
         length = len(raw_file)
+        maximum_thread = int(length / self.block_size) + 1
+
+        process_list = []
+        file_bytes = Queue()
+
         while i < length:
-            if i + 16 <= length:
-                new_file += self.aes_decrypt(raw_file[i:i+16])
-                new_file += raw_file[i+16:i+32]
-            else:
-                new_file += raw_file[i:]
-            i += 32
+            p = Process(target=self.decrypt_block, args=(raw_file[i:i+self.block_size], i, file_bytes, live_process, ))
+            p.start()
+            process_list.append(p)
+
+            i += self.block_size
+            count += 1
+
+            printed = False
+            while live_process.value > self.maximum_process:
+                time.sleep(0.2)
+                if printed is False:
+                    print("%0.2f" % (((count - 1) / maximum_thread) * 100) + "%")
+                    printed = True
+
+        while file_bytes.qsize() < count:
+            time.sleep(0.5)
+        print("복호화 완료! 결과 모으는중...")
+
+        new_file = b''
+        encrypted_files = {}
+
+        while file_bytes.empty() is not True:
+            encrypted_files.update(file_bytes.get())
+
+        i = 0
+        while i < length:
+            new_file = new_file + encrypted_files[i]
+            i += self.block_size
 
         f = open(path[:len(path.split("\\")[-1]) * -1] + path.split("\\")[-1][:path.split("\\")[-1].index(".encrypted")],
                  'wb')
